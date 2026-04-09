5d9ecc2504e20c543f58ca6adeee25780e2d9187
JS0801 <jsuthar@oncloudconsulting.ca>
2026-04-09 14:45:13 -0400

    Update from NetSuite (Auto)

--- MI_RR_Tool_SL.js
+++ MI_RR_Tool_SL.js
@@ -965,11 +965,12 @@ define(['N/ui/serverWidget', 'N/file', 'N/log', 'N/search', 'N/record', 'N/runti
     var monthQty = parseFloat(cells[SRC_MONTH_AVG_IDX_1] ? cells[SRC_MONTH_AVG_IDX_1].textContent : 0) || 0;
     var inTransit = parseFloat(cells[28] ? cells[28].textContent : 0) || 0;
     var onOrder = parseFloat(cells[33] ? cells[33].textContent : 0) || 0;
+    var committedQty = parseFloat(cells[70] ? cells[70].textContent : 0) || 0;
     var avail = parseFloat(cells[SRC_PER_AVAIL_IDX] ? cells[SRC_PER_AVAIL_IDX].textContent : 0) || 0;
 
     console.log('Values', {monthQty, inTransit, onOrder, avail})
 
-    var totalForMos = inTransit + onOrder + avail + qtyOrdered;
+    var totalForMos = inTransit + onOrder + avail + qtyOrdered - committedQty;
     var monthCell = cells[MONTH_CELL_INDEX];
     monthCell.textContent = monthQty ? (totalForMos / monthQty).toFixed(2) : '0.00';
     styleCalcCell(monthCell);
